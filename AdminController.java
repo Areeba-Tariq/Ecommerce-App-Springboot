d2064e2898d6c46804094f1c22823993df68dcec
Tahoor Asma <102382100+tahoorasma@users.noreply.github.com>
2023-11-30 22:53:57 +0500

    Missing Feature added: Delete customer button
    Delete button added to admin's customers page with implementation

--- AdminController.java
+++ AdminController.java
@@ -23,19 +23,20 @@ import com.mysql.cj.protocol.Resultset;
 
 import net.bytebuddy.asm.Advice.This;
 import net.bytebuddy.asm.Advice.OffsetMapping.ForOrigin.Renderer.ForReturnTypeName;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	
+
 	@Autowired
 	private userService userService;
 	@Autowired
 	private categoryService categoryService;
-	
+
 	@Autowired
 	private productService productService;
-	
+
 	int adminlogcheck = 0;
 	String usernameforclass = "";
 	@RequestMapping(value = {"/","/logout"})
@@ -44,9 +45,9 @@ public class AdminController {
 		usernameforclass = "";
 		return "userLogin";
 	}
-	
-	
-	
+
+
+
 	@GetMapping("/index")
 	public String index(Model model) {
 		if(usernameforclass.equalsIgnoreCase(""))
@@ -55,13 +56,13 @@ public class AdminController {
 			model.addAttribute("username", usernameforclass);
 			return "index";
 		}
-			
+
 	}
-	
-	
+
+
 	@GetMapping("login")
 	public String adminlogin() {
-		
+
 		return "adminlogin";
 	}
 	@GetMapping("Dashboard")
@@ -73,14 +74,14 @@ public class AdminController {
 	}
 	@GetMapping("/loginvalidate")
 	public String adminlog(Model model) {
-		
+
 		return "adminlogin";
 	}
 	@RequestMapping(value = "loginvalidate", method = RequestMethod.POST)
 	public ModelAndView adminlogin( @RequestParam("username") String username, @RequestParam("password") String pass) {
-		
+
 		User user=this.userService.checkLogin(username, pass);
-		
+
 		if(user.getRole().equals("ROLE_ADMIN")) {
 			ModelAndView mv = new ModelAndView("adminHome");
 			adminlogcheck=1;
@@ -106,35 +107,42 @@ public class AdminController {
 			return mView;
 		}
 	}
-	@RequestMapping(value = "categories",method = RequestMethod.POST)
-	public String addCategory(@RequestParam("categoryname") String category_name)
-	{
-		System.out.println(category_name);
-		
-		Category category =  this.categoryService.addCategory(category_name);
-		if(category.getName().equals(category_name)) {
-			return "redirect:categories";
-		}else {
-			return "redirect:categories";
+	@PostMapping("categories")
+	public String addCategory(@RequestParam("categoryname") String category_name, RedirectAttributes redirectAttributes) { //Modified
+		if (category_name.trim().isEmpty()) {
+			redirectAttributes.addFlashAttribute("errorMessage", "Category name cannot be empty.");
+			return "redirect:/admin/categories";
+		}
+
+		Category category = this.categoryService.addCategory(category_name);
+		if (category.getName().equals(category_name)) {
+			return "redirect:/admin/categories";
+		} else {
+			return "redirect:/admin/categories";
 		}
 	}
-	
+
 	@GetMapping("categories/delete")
 	public String removeCategoryDb(@RequestParam("id") int id)	//modified
-	{	
-			this.categoryService.deleteCategory(id);
-			return "redirect:/admin/categories";
-	}
-	
-	@GetMapping("categories/update")
-	public String updateCategory(@RequestParam("categoryid") int id, @RequestParam("categoryname") String categoryname)
 	{
+		this.categoryService.deleteCategory(id);
+		return "redirect:/admin/categories";
+	}
+
+	@GetMapping("categories/update")
+	public String updateCategory(@RequestParam("categoryid") int id, @RequestParam("categoryname") String categoryname, Model model,RedirectAttributes redirectAttributes) { //Modified
+		if (categoryname.trim().isEmpty()) {
+			redirectAttributes.addFlashAttribute("errorMessage", "Category name cannot be empty.");
+			model.addAttribute("categories", categoryService.getCategories());
+			return "redirect:/admin/categories";
+		}
 		Category category = this.categoryService.updateCategory(id, categoryname);
 		return "redirect:/admin/categories";
 	}
 
-	
-//	 --------------------------Remaining --------------------
+
+
+	//	 --------------------------Remaining --------------------
 	@GetMapping("products")
 	public ModelAndView getproduct() {
 		if(adminlogcheck==0){
@@ -182,7 +190,7 @@ public class AdminController {
 
 	@GetMapping("products/update/{id}")
 	public ModelAndView updateproduct(@PathVariable("id") int id) {
-		
+
 		ModelAndView mView = new ModelAndView("productsUpdate");
 		Product product = this.productService.getProduct(id);
 		List<Category> categories = this.categoryService.getCategories();
@@ -191,7 +199,7 @@ public class AdminController {
 		mView.addObject("product", product);
 		return mView;
 	}
-	
+
 	@RequestMapping(value = "products/update/{id}",method=RequestMethod.POST)
 	public String updateProduct(@PathVariable("id") int id ,@RequestParam("name") String name,@RequestParam("categoryid") int categoryId ,@RequestParam("price") int price,@RequestParam("weight") int weight, @RequestParam("quantity")int quantity,@RequestParam("description") String description,@RequestParam("productImage") String productImage)
 	{
@@ -199,19 +207,19 @@ public class AdminController {
 //		this.productService.updateProduct();
 		return "redirect:/admin/products";
 	}
-	
+
 	@GetMapping("products/delete")
 	public String removeProduct(@RequestParam("id") int id)
 	{
 		this.productService.deleteProduct(id);
 		return "redirect:/admin/products";
 	}
-	
+
 	@PostMapping("products")
 	public String postproduct() {
 		return "redirect:/admin/categories";
 	}
-	
+
 	@GetMapping("customers")
 	public ModelAndView getCustomerDetail() {
 		if(adminlogcheck==0){
@@ -225,8 +233,17 @@ public class AdminController {
 			return mView;
 		}
 	}
-	
-	
+
+	//start of code addition
+	@GetMapping("customers/delete")
+	public String removeCustomer(@RequestParam("id") int id)
+	{
+		System.out.println("Admin deleting user");
+		this.userService.deleteUser(id);
+		return "redirect:/admin/customers";
+	}
+	//end of code addition
+
 	@GetMapping("profileDisplay")
 	public String profileDisplay(Model model) {
 		String displayusername,displaypassword,displayemail,displayaddress;
@@ -237,19 +254,19 @@ public class AdminController {
 			PreparedStatement stmt = con.prepareStatement("select * from users where username = ?"+";");
 			stmt.setString(1, usernameforclass);
 			ResultSet rst = stmt.executeQuery();
-			
+
 			if(rst.next())
 			{
-			int userid = rst.getInt(1);
-			displayusername = rst.getString(2);
-			displayemail = rst.getString(3);
-			displaypassword = rst.getString(4);
-			displayaddress = rst.getString(5);
-			model.addAttribute("userid",userid);
-			model.addAttribute("username",displayusername);
-			model.addAttribute("email",displayemail);
-			model.addAttribute("password",displaypassword);
-			model.addAttribute("address",displayaddress);
+				int userid = rst.getInt(1);
+				displayusername = rst.getString(2);
+				displayemail = rst.getString(3);
+				displaypassword = rst.getString(4);
+				displayaddress = rst.getString(5);
+				model.addAttribute("userid",userid);
+				model.addAttribute("username",displayusername);
+				model.addAttribute("email",displayemail);
+				model.addAttribute("password",displaypassword);
+				model.addAttribute("address",displayaddress);
 			}
 		}
 		catch(Exception e)
@@ -259,23 +276,23 @@ public class AdminController {
 		System.out.println("Hello");
 		return "updateProfile";
 	}
-	
+
 	@RequestMapping(value = "updateuser",method=RequestMethod.POST)
-	public String updateUserProfile(@RequestParam("userid") int userid,@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("address") String address) 
-	
+	public String updateUserProfile(@RequestParam("userid") int userid,@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("address") String address)
+
 	{
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommjava","root","");
-			
+
 			PreparedStatement pst = con.prepareStatement("update users set username= ?,email = ?,password= ?, address= ? where uid = ?;");
 			pst.setString(1, username);
 			pst.setString(2, email);
 			pst.setString(3, password);
 			pst.setString(4, address);
 			pst.setInt(5, userid);
-			int i = pst.executeUpdate();	
+			int i = pst.executeUpdate();
 			usernameforclass = username;
 		}
 		catch(Exception e)
