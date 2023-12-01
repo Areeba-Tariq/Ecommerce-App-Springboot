8f0c4eb4a72e79019f12dd1db1a6512fa9bdb5d9
UsmaraSana <102314527+UsmaraSana@users.noreply.github.com>
2023-12-02 00:40:24 +0500

    Missing Feature: Add products to cart

---
+++ cartDao.java
@@ -0,0 +1,46 @@
+package com.jtspringproject.JtSpringProject.dao;
+import java.util.List;
+
+import com.jtspringproject.JtSpringProject.models.Cart;
+import com.jtspringproject.JtSpringProject.models.Category;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class cartDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf){ this.sessionFactory =sf;}
+
+    @Transactional
+    public Cart addCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().save(cart);
+        return cart;
+    }
+
+    @Transactional
+    public List<Cart> getCarts() {
+        return this.sessionFactory.getCurrentSession().createQuery("from CART").list();
+    }
+
+    @Transactional
+    public void updateCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().update(cart);
+    }
+
+    @Transactional
+    public void deleteCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().delete(cart);
+    }
+    @Transactional
+    public Cart getCartByUserId(int userId) { //New added
+        Query<?> query = this.sessionFactory.getCurrentSession()
+                .createQuery("from CART where customer_id = :userId")
+                .setParameter("userId", userId);
+        return (Cart) query.uniqueResult();
+    }
+}
